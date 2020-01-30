8011e431f14e6e82a61915db9f7405c092b1877d
abhisheksr01 <abhisheksingh2691@gmail.com>
2020-01-30 22:44:53 +0000

    fix service start failure in cloud run

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from cashier_service import app
 from cashier_service.infrastructure.event_publisher import EventPublisher
 from cashier_service.settings import config, Config
-from google.cloud import pubsub
 
 if __name__ == "__main__":
-    broker = EventPublisher(config['development'], pubsub)
+    broker = EventPublisher(config['development'])
     app.create(config=config['development'],
                broker=broker).run(
         host='0.0.0.0', port=int(Config.PORT))
